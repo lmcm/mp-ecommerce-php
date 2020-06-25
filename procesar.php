95171ce2994aa05d1e110a0982900dc6e3d1e541
Luis <luis.castillo@soytamarindo,com>
2020-06-25 15:52:23 -0500

    update

--- procesar.php
+++ procesar.php
@@ -7,6 +7,8 @@ require __DIR__ .  '/vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607"); // On Sandbox
 
+
+echo 'hola mundo';
 //MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607"); // Either Production or SandBox AccessToken
 /*
 $preference = new MercadoPago\Preference();
