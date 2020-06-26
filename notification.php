67f6d9a8dfa33465469f691a3ded8d482bc98b7c
Luis <luis.castillo@soytamarindo,com>
2020-06-26 01:00:01 -0500

    update

--- notification.php
+++ notification.php
@@ -2,6 +2,7 @@
 
  
 $data = file_get_contents('php://input');
-file_put_contents(TMP . '/data.log', print_r($data, true) . __LINE__, FILE_APPEND);
+file_put_contents(TMP . 'data.log', print_r($data, true) . __LINE__, FILE_APPEND);
+echo 'mike';
 
 ?>
