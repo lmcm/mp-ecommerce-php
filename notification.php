ba8ce7cfee17149fb8289cbe074e6b378dd38107
Luis <luis.castillo@soytamarindo,com>
2020-06-26 01:14:21 -0500

    update

--- notification.php
+++ notification.php
@@ -4,6 +4,7 @@
 $data = file_get_contents('php://input');
 file_put_contents('data.log', print_r($data, true) . __LINE__, FILE_APPEND);
 file_put_contents('data2.log', print_r(array("sistyemas"=>"kskskks"), true) . __LINE__, FILE_APPEND);
+echo file_put_contents("test.txt","Hello World. Testing!");
 
 echo 'mike';
 
