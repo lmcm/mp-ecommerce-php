374d38f15b3f10279f14185a243748a85b9f1e82
Luis <luis.castillo@soytamarindo,com>
2020-06-26 01:02:29 -0500

    update

--- notification.php
+++ notification.php
@@ -2,7 +2,9 @@
 
  
 $data = file_get_contents('php://input');
-file_put_contents(TMP . 'data.log', print_r($data, true) . __LINE__, FILE_APPEND);
+file_put_contents('data.log', print_r($data, true) . __LINE__, FILE_APPEND);
+file_put_contents('data2.log', print_r(array("sistyemas"=>"kskskks"), true) . __LINE__, FILE_APPEND);
+
 echo 'mike';
 
 ?>
