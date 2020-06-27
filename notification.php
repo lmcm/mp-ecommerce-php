fe722354408626d474418c8dce9b50efa6972e4a
Luis <luis.castillo@soytamarindo,com>
2020-06-26 22:09:10 -0500

    update

--- notification.php
+++ notification.php
@@ -28,7 +28,7 @@ if (!empty($data)) {
             //echo 'status_detail=>'.$payment['status_detail'];
             $api->request(json_encode($payment));
             //echo "DATA ID expor  " . $notification["data"]["id"];
-            echo var_export($payment);
+            echo $payment;
             break;
     }
 }
