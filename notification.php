fbc534e1bb10c8ab715ca1106315c945cb2a2469
Luis <luis.castillo@soytamarindo,com>
2020-06-26 20:46:32 -0500

    update

--- notification.php
+++ notification.php
@@ -5,9 +5,9 @@ $data = file_get_contents('php://input');
 //($api->request($data);
 echo $data;
 if (!empty($data)) {
-    //$notification = json_decode($data);
+    $notification = json_decode($data);
     //MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607");
-    echo "notificacoon"; //. $notification["action"];
+    echo "notificacoon 2"; //. $notification["action"];
     /*switch ($notification["action"]) {
         case 'payment.created':
             echo "DATA ID ". $notification["data"]["id"];
