7c409e60f92cff514d759db34be58af17eb78427
Luis <luis.castillo@soytamarindo,com>
2020-06-26 20:40:52 -0500

    update

--- notification.php
+++ notification.php
@@ -6,13 +6,13 @@ $data = file_get_contents('php://input');
 
 if (!empty($data)) {
     $notification = json_decode($data);
-    MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607");
+    //MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607");
     echo "notificacoon" . $notification["action"];
     switch ($notification["action"]) {
         case 'payment.created':
             echo "DATA ID ". $notification["data"]["id"];
-            $payment = MercadoPago\Payment().find_by_id($notification["data"]["id"]);
-            echo var_dump($payment);
+           // $payment = MercadoPago\Payment().find_by_id($notification["data"]["id"]);
+            //echo var_dump($payment);
             //$api->request($payment);
             break;
     }
