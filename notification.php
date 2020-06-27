2c8da21485f343061082d95389f8a7bbff0137cf
Luis <luis.castillo@soytamarindo,com>
2020-06-27 16:35:47 -0500

    update

--- notification.php
+++ notification.php
@@ -20,7 +20,9 @@ $api->request($data);
 //MercadoPago\SDK::setIntegratorId("INTEGRATOR_ID");
 if (!empty($data)) {
     $notification = json_decode($data, true);
-    MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607");
+    //MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607");
+    MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
+    MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
     switch ($notification["action"]) {
         case 'payment.created':
             //echo "DATA ID " . $notification["data"]["id"];
@@ -29,7 +31,7 @@ if (!empty($data)) {
             //echo 'status_detail=>'.$payment['status_detail'];
             //$api->reques($payment);
             //echo "DATA ID expor  " . $notification["data"]["id"];
-            echo var_dum($payment);
+            //echo var_dum($payment);
             break;
     }
 }
--- procesar.php
+++ procesar.php
@@ -1,9 +1,8 @@
 <?php
 
 require __DIR__ .  '/vendor/autoload.php';
-MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607");
-
-//MercadoPago\SDK::setIntegratorId("INTEGRATOR_ID");
+MercadoPago\SDK::setAccessToken("APP_USR-8058997674329963-062418-89271e2424bb1955bc05b1d7dd0977a8-592190948");
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 
 $preference = new MercadoPago\Preference();
