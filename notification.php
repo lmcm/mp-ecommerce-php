f8d31e4fab7642410dfd1f7f16c6604652356b76
Luis <luis.castillo@soytamarindo,com>
2020-06-26 21:24:29 -0500

    update

--- notification.php
+++ notification.php
@@ -9,7 +9,7 @@ if (!empty($data)) {
     MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607");
     switch ($notification["action"]) {
         case 'payment.created':
-            echo "DATA ID " . $notification["data"]["id"];
+            //echo "DATA ID " . $notification["data"]["id"];
             $payment = MercadoPago\Payment::find_by_id($notification["data"]["id"]);
             echo 'status=>'.$payment['status'].'</br>';
             echo 'status_detail=>'.$payment['status_detail'];
@@ -20,13 +20,12 @@ if (!empty($data)) {
 
 class API
 {
-    public function request($data)
+    public function request($info)
     {
-
         $handle = curl_init('https://hookb.in/oXVXXLQkPDu1mmLaRmqM');
         curl_setopt($handle, CURLOPT_POST, 1);
-        curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($handle, CURLOPT_POSTFIELDS, $info);
         curl_setopt($handle, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-        $result = curl_exec($handle);
+        curl_exec($handle);
     }
 }
