2aed105e6e96f76749f3c210497e6159b1d018e0
Luis <luis.castillo@soytamarindo,com>
2020-06-26 20:43:07 -0500

    update

--- notification.php
+++ notification.php
@@ -3,19 +3,19 @@ require __DIR__ . '/vendor/autoload.php';
 $data = file_get_contents('php://input');
 //$api = new API();
 //($api->request($data);
-
+echo $data;
 if (!empty($data)) {
-    $notification = json_decode($data);
+    //$notification = json_decode($data);
     //MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607");
-    echo "notificacoon" . $notification["action"];
-    switch ($notification["action"]) {
+    echo "notificacoon"; //. $notification["action"];
+    /*switch ($notification["action"]) {
         case 'payment.created':
             echo "DATA ID ". $notification["data"]["id"];
            // $payment = MercadoPago\Payment().find_by_id($notification["data"]["id"]);
             //echo var_dump($payment);
             //$api->request($payment);
             break;
-    }
+    }*/
 }
 /*
 class API
