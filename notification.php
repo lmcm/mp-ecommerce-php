89960b896840f0b48c84765ee06f6011cb447fde
Luis <luis.castillo@soytamarindo,com>
2020-06-26 20:55:56 -0500

    update

--- notification.php
+++ notification.php
@@ -1,25 +1,22 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 $data = file_get_contents('php://input');
-//$api = new API();
-//($api->request($data);
-echo $data;
+$api = new API();
+$api->request($data);
+
 if (!empty($data)) {
-    $notification = json_decode($data,true);
+    $notification = json_decode($data, true);
     MercadoPago\SDK::setAccessToken("TEST-7927007857849250-062405-afb9bb9c52903c3deb1ad9e8116e5aa0-590656607");
-    echo "---11--";
-    echo $notification["action"];
-    echo "---2222--";
-    /*switch ($notification["action"]) {
+    switch ($notification["action"]) {
         case 'payment.created':
-            echo "DATA ID ". $notification["data"]["id"];
-           // $payment = MercadoPago\Payment().find_by_id($notification["data"]["id"]);
-            //echo var_dump($payment);
-            //$api->request($payment);
+            echo "DATA ID " . $notification["data"]["id"];
+            $payment = MercadoPago\Payment() . find_by_id($notification["data"]["id"]);
+            echo var_dump($payment);
+            $api->request($payment);
             break;
-    }*/
+    }
 }
-/*
+
 class API
 {
     public function request($data)
@@ -31,4 +28,4 @@ class API
         curl_setopt($handle, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
         $result = curl_exec($handle);
     }
-}*/
+}
