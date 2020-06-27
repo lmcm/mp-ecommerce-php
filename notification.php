b3204923f8868e35a738bdd8b6a3e46089f4d099
Luis <luis.castillo@soytamarindo,com>
2020-06-26 23:45:24 -0500

    update

--- notification.php
+++ notification.php
@@ -26,7 +26,7 @@ if (!empty($data)) {
             $payment = MercadoPago\Payment::get($notification["data"]["id"]);//find_by_id($notification["data"]["id"]);
             //echo 'status=>'.$payment['status'].'</br>';
             //echo 'status_detail=>'.$payment['status_detail'];
-            $api->request(json_encode($payment));
+            $api->reques($payment);
             //echo "DATA ID expor  " . $notification["data"]["id"];
             echo var_dum($payment);
             break;
